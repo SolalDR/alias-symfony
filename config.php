5fcb5ee2f5772d8b97c5dd2f0b054db4cdc70bc5
SolalDR <solal.dussout-revel@hotmail.fr>
2016-12-09 19:35:57 +0100

    Add command like rails to json

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 //Votre chemin vers le dossier du script
-$dirPath = "/root/myUser/path/to/directory/alias-symfony";
+$dirPath = "/Users/Solal/Documents/Developpement/alias-symfony";
 
 //Prefix de la commande, utiliser app/console au lieu de bin/console pour les versions de Symfony inferieur à 3.0.
 $prefixCommand = "php bin/console";
